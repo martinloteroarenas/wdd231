0b1e6a7dd4af731142adbeb9167aac68cca105c2
Martin Lotero Arenas <martiloteroarenas@gmail.com>
2026-05-15 21:56:49 -0500

    w01 assignment

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,8 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+const year = document.querySelector("#year");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
 const navbutton = document.querySelector('#ham-btn');
 
 navbutton.addEventListener('click', () => {
