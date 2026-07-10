db66c68786b731b50e692d36fad6d0846a5b2cb6
Martin Lotero Arenas <martiloteroarenas@gmail.com>
2026-07-10 14:46:06 -0500

    nav changed and interacts with the hambutton

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,7 +4,9 @@ const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 const navbutton = document.querySelector('#ham-btn');
+const mainNav = document.querySelector('#hide-menu');
 
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
+    mainNav.classList.toggle('show-menu');
 });
