89fcee9e74562862bbc7d90848e7aeb3b7d22eed
Martin Lotero Arenas <martiloteroarenas@gmail.com>
2026-08-01 18:20:47 -0500

    va bien

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -4,3 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
         timestampInput.value = new Date().toISOString();
     }
 });
+
+const openButtons = document.querySelectorAll('.benefit-btn');
+const closeButtons = document.querySelectorAll('.close-modal');
+
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.querySelector(`#${modalId}`);
+        if (modal) {
+            modal.showModal();
+        }
+    });
+});
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const modal = e.target.closest('dialog');
+        modal.close();
+    });
+});
+
+document.querySelectorAll('dialog').forEach(modal => {
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.close();
+        }
+    });
+});
