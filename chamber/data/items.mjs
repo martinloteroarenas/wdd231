5f7bf3250ebeadb57bf1946f6bd8e6d46a2fc1c1
Martin Lotero Arenas <martiloteroarenas@gmail.com>
2026-08-07 18:32:51 -0500

    .mjs

---
+++ chamber/data/items.mjs
@@ -0,0 +1,60 @@
+export default {
+    "items": [
+        {
+            "title": "Ukumari Zoo",
+            "address": "Entrance through the Santa Barbara Service Station, Kilometer 14, Pereira - Cartago, CERRITOS, Risaralda",
+            "description": "Ecotourism & wildlife park with native & African animals & plants on display, plus food vendors.",
+            "url": "https://ukumari.org/",
+            "photo": "../chamber/images/ukumari.webp"
+        },
+        {
+            "title": "Consota",
+            "address": "Vía Parque Consotá-Galicia, Pereira, Risaralda, Q5XV+J7",
+            "description": "It stands out for its swimming pools and water slides, sports areas, ecological trails, lodging options, and cultural replicas like its Villa Paisa. It is one of the most popular family and tourist destinations in the region.",
+            "url": "https://comfamiliar.com/parque-consota/",
+            "photo": "../chamber/images/consota.webp"
+        },
+        {
+            "title": "Hernán Ramírez Villegas Stadium",
+            "address": "vía, Villa Olímpica, Pereira, Risaralda",
+            "description": "People say this stadium has excellent, well-maintained facilities and is a great place for sports, concerts, and large gatherings. They also highlight the beautiful surroundings, which are ideal for exercising, running, and family walks.",
+            "url": "https://www.google.com/maps/place/estadio+hern%C3%A1n+ram%C3%ADrez+villegas/data=!4m2!3m1!1s0x8e387dc9ce68532b:0xf418b4eb3e434a10?sa=X&ved=1t:242&ictx=111",
+            "photo": "../chamber/images/estadio.webp"
+        },
+        {
+            "title": "Pereira Art Museum",
+            "address": "Av. de las Américas #19-88, Pereira, Risaralda",
+            "description": "Striking venue with exhibitions of modern & contemporary work, plus film screenings & a cafe.",
+            "url": "https://www.museodeartedepereira.com/",
+            "photo": "../chamber/images/museo.webp"
+        },
+        {
+            "title": "Cerro Canceles",
+            "address": "calle 2a Este, Pereira, Risaralda",
+            "description": "this viewpoint offers spectacular 360-degree views of Pereira and Dosquebradas, with vendors selling coffee, cold drinks, and snacks.",
+            "url": "https://www.google.com/maps/place/Cerro+Canceles/@4.801963,-75.6751731,17z/data=!4m14!1m7!3m6!1s0x8e388791576b94ab:0x856c5ce4b62a2b09!2sCerro+Canceles!8m2!3d4.801963!4d-75.6733524!16s%2Fg%2F11qnk8xtqp!3m5!1s0x8e388791576b94ab:0x856c5ce4b62a2b09!8m2!3d4.801963!4d-75.6733524!16s%2Fg%2F11qnk8xtqp?entry=ttu&g_ep=EgoyMDI2MDgwNS4xIKXMDSoASAFQAw%3D%3D",
+            "photo": "../chamber/images/canceles.webp"
+        },
+        {
+            "title": "natural hot springs of Santa Rosa de Cabal",
+            "address": "Km 10 Vereda San Ramón, Pereira, Santa Rosa de Cabal, Risaralda",
+            "description": "this destination offers stunning natural hot springs and a beautiful waterfall, creating a magical and relaxing atmosphere. They also highlight the comfortable, clean rooms, delicious food, and attentive staff. ",
+            "url": "https://www.google.com/maps/place/Hotel+Termales+Santa+Rosa+de+Cabal/@4.8420784,-75.5557023,17z/data=!4m9!3m8!1s0x8e3883aec914e46d:0x105cdc905df715f0!5m2!4m1!1i2!8m2!3d4.8376505!4d-75.5489374!16s%2Fg%2F11crzrhsqy?entry=ttu&g_ep=EgoyMDI2MDgwNS4xIKXMDSoASAFQAw%3D%3D",
+            "photo": "../chamber/images/termales.webp"
+        },
+        {
+            "title": "Arboleda Park Mall",
+            "address": "Cra. 14 #5-20, Pereira, Risaralda",
+            "description": "Large shopping mall featuring brand-name retailers, a food court & a multiplex movie theater.",
+            "url": "https://www.parquearboleda.com/",
+            "photo": "../chamber/images/arboleda.webp"
+        },
+        {
+            "title": "San Jose Church",
+            "address": "Cra. 13 #15-50, Pereira, Risaralda",
+            "description": "this church features beautiful Gothic-style architecture, including impressive stained-glass windows and an elegant stone structure.",
+            "url": "https://www.google.com/maps/place/IGLESIA+SAN+JOSE+-+PEREIRA,+RISARALDA/@4.8090448,-75.6915423,18.67z/data=!4m14!1m7!3m6!1s0x8e388748c4380771:0xd6fa8947dd7fe055!2sOur+Lady+of+Poverty+Cathedral,+Pereira!8m2!3d4.8146415!4d-75.6951458!16s%2Fg%2F12365gpm!3m5!1s0x8e3887476966551b:0x105f5601eb538b2b!8m2!3d4.8093098!4d-75.6916408!16s%2Fg%2F11b7q5rt4_?entry=ttu&g_ep=EgoyMDI2MDgwNS4xIKXMDSoASAFQAw%3D%3D",
+            "photo": "../chamber/images/iglesia.webp"
+        },
+    ]
+};
